33a5b2c6efed1ec6cf8e57cd60b6d4c36693cea6
evycham <ygruzevich8484@gmail.com>
2026-03-20 18:09:14 +0100

    init

--- app/src/main/java/com/example/mob_systeme1/MainActivity.kt
+++ app/src/main/java/com/example/mob_systeme1/MainActivity.kt
@@ -7,16 +7,40 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+    fun init(){
+        val display = findViewById<TextView>(R.id.tvDisplay)
+        val btnMR = findViewById<Button>(R.id.btnMR)
+        val btnMS = findViewById<Button>(R.id.btnMS)
+        val btnClean = findViewById<Button>(R.id.btnClean)
+        val btnDelete = findViewById<Button>(R.id.btnDelete)
+        val btn9 = findViewById<Button>(R.id.btn9)
+        val btn8 = findViewById<Button>(R.id.btn8)
+        val btn7 = findViewById<Button>(R.id.btn7)
+        val btn6 = findViewById<Button>(R.id.btn6)
+        val btn5 = findViewById<Button>(R.id.btn5)
+        val btn4 = findViewById<Button>(R.id.btn4)
+        val btn3 = findViewById<Button>(R.id.btn3)
+        val btn2 = findViewById<Button>(R.id.btn2)
+        val btn1 = findViewById<Button>(R.id.btn1)
+        val btn0 = findViewById<Button>(R.id.btn0)
+        val btnDot = findViewById<Button>(R.id.btnDot)
+        val btnDivide = findViewById<Button>(R.id.btnDivide)
+        val btnMultiply = findViewById<Button>(R.id.btnMultiply)
+        val btnMinus = findViewById<Button>(R.id.btnMinus)
+        val btnPlus = findViewById<Button>(R.id.btnPlus)
+        val btnEquals = findViewById<Button>(R.id.btnEquals)
     }
 }
